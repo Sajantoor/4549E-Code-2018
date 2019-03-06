89f8f11d5cb9b6244e47ff9647e121690d31eca9
Sajan Toor <sajandeep.toor@surreyschools.ca>
2019-03-06 15:44:30 -0800

    Made some changes

---
+++ include/variables.h
@@ -0,0 +1,10 @@
+#include "main.h"
+extern pros::Motor left_mtr;
+extern pros::Motor leftfront_mtr;
+extern pros::Motor right_mtr;
+extern pros::Motor rightfront_mtr;
+extern pros::Motor pivot_mtr;
+extern pros::Motor intake_mtr;
+extern pros::Motor puncher_mtr;
+extern pros::ADIAnalogIn sensor;
+extern pros::Motor lift_mtr;
